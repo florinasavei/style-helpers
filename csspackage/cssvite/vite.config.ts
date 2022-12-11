075d4ec966ac5b62b5f118ed97568d838c597d68
florin.asavei <florin.asavei@exghts.com>
2022-12-11 22:56:57 +0200

    removed react dependency

--- csspackage/cssvite/vite.config.ts
+++ csspackage/cssvite/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 
 // https://vitejs.dev/config/
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.tsx"),
